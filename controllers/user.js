03102ac57563bf2e27ff87d730deb10b332befc6
DiBarrera <di_barrera@hotmail.com>
2021-04-11 12:30:40 -0500

    Configuracion para registrar nuevos usuarios, condicionales, poblado de base de datos exitoso

--- controllers/user.js
+++ controllers/user.js
@@ -27,6 +27,34 @@ function signUp(req, res) {
             
             bcrypt.hash(password, null, null, function(err, hash) {
                 console.log("Encriptado contraseña")
+                if(err) {
+                    console.log(`Este es el error 1 ---> ${err}`)
+                    res.status(500).send({message: "Error al encriptar la contraseña"})
+                } else {
+                    console.log("Continuar 3")
+                    // res.status(200).send({ message: hash})   // <--- ESTE VA COMENTADO POR QUE INTERFIERE CON LA LINEA 53 DE ALGUNA FORMA. ARROJA EL ERROR: Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
+                    console.log("Asignando Hash a password de usuario")
+                    console.log(`constrasñea de usuario = ${hash}`)
+                    console.log(hash)
+                    user.password = hash
+
+                    console.log("Registrando usuario con user.save")
+                    user.save((err, userStored) => {
+                        if(err) {
+                            console.log(`Este es el error 2 ---> ${err}`)
+                            res.status(500).send({message: "El usuario ya existe"})
+                        } else {
+                            console.log("Continuar 4")
+                            if(!userStored) {
+                                res.status(404).send({message: "Error al crear el usuario"})
+                            } else {
+                                console.log("Continuar 5")
+                                console.log(`Este es el usuario Storeado ---> ${userStored}`)
+                                res.status(200).send({user: userStored})
+                            }
+                        }
+                    })
+                }
             })
             // res.status(200).send({message: "Usuario creado"})
         }
