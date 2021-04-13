7cdcc3d917d176db923700c45deb580614643765
DiBarrera <di_barrera@hotmail.com>
2021-04-12 19:11:17 -0500

    Headers reforzados en el servidor

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ mongoose.set("useFindAndModify", false);
 mongoose.set("useCreateIndex", true);
 
 app.get("/api/v1/", (req,res) => {
-    res.send("Middlewares para bloquear url a usuarios no ingresados o logeados")
+    res.send("Headers reforzados en el servidor")
 })
 
 mongoose.connect(
