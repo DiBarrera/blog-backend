8e3f57a9f8115d4b84b1876e8c2c2dcfcf0de1ee
DiBarrera <di_barrera@hotmail.com>
2021-04-13 20:15:21 -0500

    Función incompleta para subir avatar a usuario, Endpoint para subir la imagen al server probada, rotueo y middlewareo y condicionales para subir la imagen incompletas

--- routers/user.js
+++ routers/user.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const UserController = require("../controllers/user");
 const md_auth = require("../middleware/authenticadted");
-const multipart = require("connect-multiparty");
-const md_upload_avatar = multipart({uploadDir: "./uploads/avatar"})
 
 const api = express.Router();
 
 api.post("/sign-up", UserController.signUp);
 api.post("/sign-in", UserController.signIn);
-api.get("/users", [md_auth.ensureAuth], UserController.getUsers);
-api.get("/users-active", [md_auth.ensureAuth, md_upload_avatar], UserController.getUsersActive);
-api.put("/upload-avatar/:id", [md_auth.ensureAuth], UserController.uploadAvatar);
+api.get("/users", [md_auth.ensureAuth], UserController.getUsers)
+api.get("/users-active", [md_auth.ensureAuth], UserController.getUsersActive)
 
 module.exports = api;
