543e514bd9b4eda45427ad6c68923cbc6e66f3f1
DiBarrera <di_barrera@hotmail.com>
2021-04-16 09:17:24 -0500

    Intentando Deploy Heroku

--- index.js
+++ index.js
@@ -7,11 +7,29 @@ mongoose.set("useFindAndModify", false);
 mongoose.set("useCreateIndex", true);
 
 app.get("/api/v1/", (req,res) => {
-    res.send("Endpoint para crear Usuarios desde el Dashboard de Admin, test exitoso en Postman, el usuario puede hacer LogIn exitoso en la App")
+    res.send("Intentando Deploy Heroku")
 })
 
+// mongoose.connect(
+//     `mongodb://${IP_SERVER}:${PORT_DB}/webpersonalblog`,
+//     { useNewUrlParser: true, useUnifiedTopology: true }, (err, res) => {
+//         if(err) {
+//             throw err
+//         } else {
+//             console.log("La conexion a la base de datos es correcta.")
+
+//             app.listen(port, () => {
+//                 console.log("######################");
+//                 console.log("###### API REST ######");
+//                 console.log("######################");
+//                 console.log(`http://${IP_SERVER}:${port}/api/${API_VERSION}/`);
+//             })
+//         }
+//     }
+// );
+
 mongoose.connect(
-    `mongodb://${IP_SERVER}:${PORT_DB}/webpersonalblog`,
+    `mongodb+srv://diegobarrera:Holamundo2021@cluster0.ubmvm.mongodb.net/webpersonaltipoblog`,
     { useNewUrlParser: true, useUnifiedTopology: true }, (err, res) => {
         if(err) {
             throw err
